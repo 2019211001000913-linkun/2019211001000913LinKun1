723757c009b20ab49ec361a2f600120cee1966c8
ASUS <2557467717@qq.com>
2021-05-25 20:22:52 +0800

    HW-Week 12 Commit

--- src/main/java/com/linkun/controller/ProductListServlet.java
+++ src/main/java/com/linkun/controller/ProductListServlet.java
@@ -1,14 +1,32 @@
 package com.linkun.controller;
 
-import javax.servlet.*;
-import javax.servlet.http.*;
-import javax.servlet.annotation.*;
+import com.linkun.dao.ProductDao;
+import com.linkun.model.Product;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.Connection;
+import java.util.List;
 
 @WebServlet(name = "ProductListServlet", value = "/admin/productList")
 public class ProductListServlet extends HttpServlet {
+
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        try {
+            ProductDao productDao = new ProductDao();
+            List<Product> productList = productDao.findAll((Connection) getServletContext().getAttribute("con"));
+            System.out.println(productList);
+            request.setAttribute("productList", productList);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
         request.getRequestDispatcher("/WEB-INF/views/admin/productList.jsp").forward(request, response);
     }
 
